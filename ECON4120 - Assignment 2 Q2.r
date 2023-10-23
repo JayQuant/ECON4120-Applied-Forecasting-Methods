e592b689d1f3702611ae859cd7c79d8418a2bb10
Jay Jung <132352116+JayQuant@users.noreply.github.com>
2023-10-23 18:01:10 +0800

    Update ECON4120 - Assignment 2 Q2.r
    edit for better visibility

--- ECON4120 - Assignment 2 Q2.r
+++ ECON4120 - Assignment 2 Q2.r
@@ -4,7 +4,6 @@ library(haven)
 library(tidyverse)
 library(fpp3)
 library(zoo)
-library(patchwork)
 library(tidyquant)
 
 par(mfrow=c(2,1))
@@ -12,37 +11,47 @@ par(mfrow=c(2,1))
 US_QGDP <- read_dta("C:/Users/idkup/Desktop/Course studies/2022 TERM 2/ECON4120/US_QGDP.dta")
 view(US_QGDP)
 
-##Manipulation and Cleaning Data
+## Manipulation and Cleaning Data
 
-sum(is.na(US_QGDP)) #check for NA values
-US_QGDP$gdp <- format(round(US_QGDP$gdp,3), nsmall=3) #Change GDP column so all three decimal places are shown
+# Check for NA values
+sum(is.na(US_QGDP)) 
 
-ti <- seq(as.Date("1947-01-01"), as.Date("2019-12-01"), by= "quarter") #Create quarterly data sequence
-length(ti) == length(US_QGDP$gdp)    #Check time index length is same as that of data
+# Change GDP column so all three decimal places are shown
+US_QGDP$gdp <- as.numeric(US_QGDP$gdp)
+US_QGDP$gdp <- format(round(US_QGDP$gdp,3), nsmall=3)
 
-ti <- as.yearqtr(ti, format="%Y-%m-%d") #Change to quarterly data
+# Create quarterly data sequence
+ti <- seq(as.Date("1947-01-01"), as.Date("2019-12-01"), by= "quarter")
+
+# Check time index length is same as that of data
+length(ti) == length(US_QGDP$gdp)    
+
+# Change to quarterly data
+ti <- as.yearqtr(ti, format="%Y-%m-%d") 
 ti <- yearquarter(ti)
 US_QGDP$yq <- ti
 
-US_QGDP$gdp <- as.numeric(US_QGDP$gdp) #change character to numeric vector
+# Change character to numeric vector
+US_QGDP$gdp <- as.numeric(US_QGDP$gdp)
 
+# Swap column order, change to time series object
 US_QGDP <- select(US_QGDP, yq, gdp) %>%
-	as_tsibble(index = yq)		#swap column order, change to time series object
+	as_tsibble(index = yq)		
 
 autoplot(US_QGDP) #plot time series of US GDP 
 
-###Q1
+### Q1
 
-##i) detecting stationarity of series visually
+##i) Detecting stationarity of series visually
 autoplot(US_QGDP) #plot time series of US GDP 
 
-##ii) differencing the series by order 1
+##ii) Differencing the series by order 1
 plt.d.US_QGDP <- US_QGDP %>% transmute(
 	'd.gdp' = difference(gdp)
 	) %>%
 	autoplot()
 
-##iii) differencing the log series by order 1
+##iii) Differencing the log series by order 1
 
 plt.d.logUS_QGDP <- US_QGDP %>% transmute(
 	'd.loggdp' = difference(log(gdp))
@@ -52,29 +61,29 @@ plt.d.logUS_QGDP <- US_QGDP %>% transmute(
 	geom_line(col='steelblue') +
 	labs( y = "first difference of log gdp", title = "Growth rate of quarterly GDP")
 
-#Show differenced series on top and differenced log series on bottom
+# Show differenced series on top and differenced log series on bottom
 plt.d.US_QGDP / plt.d.logUS_QGDP
 
 ##iv) Estimate AR(1) model for GDP growth rate
 
-#Save differenced log series
+# Save differenced log series
 d.logUS_QGDP <- US_QGDP %>% transmute(
 	'd.loggdp' = difference(log(gdp))
 	)
 	
 
 
-#Fit AR(1) model
+# Fit AR(1) model
 fit <- d.logUS_QGDP %>% drop_na() %>%
 	model(AR(d.loggdp ~ order(1), na.rm=T)
 	)
 
 report(fit)
 
-#forecast 1 quarter ahead
+# Forecast 1 quarter ahead
 fit %>% forecast(h = 1)
 
-#Plot forecasted path and distribution along with original data
+# Plot forecasted path and distribution along with original data
 forecast.1 <- fit %>% forecast() %>% autoplot(d.logUS_QGDP) + 
 	coord_x_date(xlim = c("2000-1-1" , "2021-1-1"))
 
@@ -87,17 +96,10 @@ fit2 <- d.logUS_QGDP %>% drop_na() %>%
 	
 report(fit2)
 
-#Forecast 1 quarter ahead
+# Forecast 1 quarter ahead
 fit2 %>% forecast(h = 1)
 
-#Plot forecasted path along original data
+# Plot forecasted path along original data
 forecast.2 <- fit2 %>% forecast() %>% autoplot(d.logUS_QGDP) + 
 	coord_x_date(xlim = c("2000-1-1" , "2021-1-1"))
 	
-	
-
-
-## Using patchwork to display two series in same panel
-# First, create ggplot of two series (x1 and x2 used for illustrative purpose)
-
-p1 <- ggplot(data.frame(x = x1), aes(x = x)) + geom_line()
